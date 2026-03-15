a1d550452f63acbc9489b53ad43ae078a9514623
Anubhav Rawat <anubhav2021rawat@gmail.com>
2026-03-15 16:08:36 +0530

    feat: implement authentication and credential registration

---
+++ src/app/api/auth/register/route.ts
@@ -0,0 +1,36 @@
+import { NextResponse } from "next/server";
+import bcrypt from "bcrypt";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient({
+  datasourceUrl: "file:./dev.db",
+});
+
+export async function POST(request: Request) {
+  try {
+    const body = await request.json();
+    const { name, email, password, role } = body;
+
+    if (!name || !email || !password) {
+      return new NextResponse("Missing Info", { status: 400 });
+    }
+
+    const _role = role === "ADMIN" ? "ADMIN" : "STUDENT";
+
+    const hashedPassword = await bcrypt.hash(password, 12);
+
+    const user = await prisma.user.create({
+      data: {
+        email,
+        name,
+        hashed_password: hashedPassword,
+        role: _role,
+      },
+    });
+
+    return NextResponse.json(user);
+  } catch (error: any) {
+    console.error(error, "REGISTRATION_ERROR");
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
---
+++ src/lib/auth.ts
@@ -0,0 +1,75 @@
+import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcrypt";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "email" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
+          throw new Error("Invalid credentials");
+        }
+
+        const user = await prisma.user.findUnique({
+          where: {
+            email: credentials.email,
+          },
+        });
+
+        if (!user || !user?.hashed_password) {
+          throw new Error("Invalid credentials");
+        }
+
+        const isCorrectPassword = await bcrypt.compare(
+          credentials.password,
+          user.hashed_password
+        );
+
+        if (!isCorrectPassword) {
+          throw new Error("Invalid credentials");
+        }
+
+        return {
+          id: user.id,
+          name: user.name,
+          email: user.email,
+          role: user.role,
+        };
+      },
+    }),
+  ],
+  pages: {
+    signIn: "/login",
+  },
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        // @ts-ignore
+        token.role = user.role;
+        token.id = user.id;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      if (session.user) {
+        // @ts-ignore
+        session.user.role = token.role;
+        // @ts-ignore
+        session.user.id = token.id;
+      }
+      return session;
+    },
+  },
+  session: {
+    strategy: "jwt",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+};
